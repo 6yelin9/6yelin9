455a192687d1e70a3f4f8f9f4aa67cad47f16dc0
6yelin9 <6yelin9@gmail.com>
2024-08-15 00:36:44 +0900

    .

--- test_code/main.cpp
+++ test_code/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 using namespace std;
 
-// set variables
-
 int main()
 {
-    // code
+    int age = 20;
+    string name = "Yelin";
+
+    cout << "Hello, my name is " << name << " and I am " << age << " years old." << endl;
+    
+    /* Output:
+        Hello, my name is Yelin and I am 20 years old.
+    */
+
+    return 0;
 }
