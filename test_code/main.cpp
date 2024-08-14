1407d856325c761252e4079310c91ec79645fd64
6yelin9 <6yelin9@gmail.com>
2024-08-15 01:26:43 +0900

    .

--- test_code/main.cpp
+++ test_code/main.cpp
@@ -3,13 +3,21 @@ using namespace std;
 
 int main()
 {
-    int a = 10;
-    int b = 20;
+    int a, b;
 
+    cout << "[Input]" << endl;
+    cout << "a: ", cin >> a;
+    cout << "b: ", cin >> b;
+
+    cout << "[Output]" << endl;
     cout << a << " + " << b << " = " << a + b << endl;
 
-    /* Output:
-    10 + 20 = 30
+    /*
+    [Input]
+    a: 10
+    b: 20
+    [Output]
+    10 + 20 = 30    
     */
 
     return 0;
